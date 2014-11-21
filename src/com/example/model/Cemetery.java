5251e2a27812a0c7a6895c4cfbefe14e1e3203ca
NGUYEN THE BAO <bao1tb@yahoo.com>
2014-11-21 11:06:02 +0700

    update 21/11

---
+++ src/com/example/model/Cemetery.java
@@ -0,0 +1,26 @@
+package com.example.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Cemetery {
+	private String name,address;
+	private LatLng latLng;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public LatLng getLatLng() {
+		return latLng;
+	}
+	public void setLatLng(LatLng latLng) {
+		this.latLng = latLng;
+	}
+}
